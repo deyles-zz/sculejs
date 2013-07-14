19ceb0945321925ea79c63be86fe870f9edf0d7b
Dan <daniel.eyles@yahoo.com.au>
2013-07-14 18:56:27 +1000

    forgot tests for ticket #29

---
+++ tests/Ticket29Tests.js
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2013, Dan Eyles (dan@irlgaming.com)
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of IRL Gaming nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL IRL Gaming BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+var sculedb = require('../lib/com.scule.db');
+
+exports['HashTable hasOwnProperty'] = function(beforeExit, assert) {
+
+    var ht = sculedb.Scule.$d.getHashTable();
+    ht.put('hasOwnProperty', true);
+    ht.put('bar', 'foo');    
+        
+    assert.equal(null, ht.get('foo'));    
+    assert.equal(null, ht.search('foo'));    
+    assert.equal(false, ht.contains('foo'));
+    assert.equal(true, ht.contains('hasOwnProperty'));
+    assert.equal(true, ht.get('hasOwnProperty'));
+    assert.equal(true, ht.contains('bar'));
+    assert.equal('foo', ht.get('bar'));
+    assert.equal(2, ht.getKeys().length);
+    assert.equal('[true,"foo"]', JSON.stringify(ht.getValues()));
+
+};
